a0918dfdaa310906e0d91b77d180e2d5d93197a0
phuhoang <phuhoang@sina.com>
2018-04-28 17:32:16 +0800

    删除dist-test

--- dist-test/assets/js/ie-lte-9.js
+++
@@ -1,4 +0,0 @@
-let ieElement = document.createElement('div');
-ieElement.setAttribute('style', 'width: 100%; line-height: 64px; background: #ccc;position: fixed; top: 0;z-index:99; text-index: 20px;');
-ieElement.innerText = '建议使用Chrome浏览器';
-document.getElementsByTagName('body')[0].appendChild(ieElement);
